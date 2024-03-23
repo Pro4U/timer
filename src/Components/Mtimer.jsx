9cfd491381ed125452556a91d1bbaf80778ad89b
shivam <stripathi9871@gmail.com>
2024-03-23 20:58:18 +0530

    done!

--- src/Components/Mtimer.jsx
+++ src/Components/Mtimer.jsx
@@ -3,19 +3,42 @@ import React, { useEffect, useState } from 'react'
 function Mtimer() {
    
     let [count,setCount] = useState(0);
+    let [running,setRunning] = useState(false);
 
-    useEffect(function(){
-        setCount(count+1);
-    },[])
+    useEffect(()=>{
+
+      let id;
+
+      if(running){
+        id = setInterval(() => {
+          setCount(count + 1);
+        }, 1000);
+
+      }
+      return () => {
+        clearInterval(id);
+      };
+
+    }, [count, running]);
+
+  function handleStart(){
+      setRunning(true);
+    }
+
+  function handleStop(){
+    setRunning(false);
+    setCount(0);
+  }
 
   return (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <div style={{border: "1px solid black",margin:"10px",
-              padding: "10px", width: "10vw"
-          }}>{count}</div>
-          <button>Start</button>
-          <button>Stop</button>
-    </div>
+                padding: "10px", width: "10vw"
+            }}>{count}
+        </div>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+      </div>
   )
 }
 
